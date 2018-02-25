fe5fbd5bcd42d08eeb4ec6c9c9979e6aa9d8dbf3
Sven Liebig <liebigsv@gmail.com>
2018-02-25 17:42:19 +0100

    docker compose and postgres starts working

---
+++ db/1-schema.sql
@@ -0,0 +1,16 @@
+create table userssss
+(
+	id INT,
+	email VARCHAR
+);
+
+create table goalsssss
+(
+	id INT,
+	name VARCHAR,
+	description VARCHAR
+);
+
+INSERT INTO userssss
+VALUES
+	(1, 'Hello@mail.de');
---
+++ db/init.sql
@@ -0,0 +1,16 @@
+create table users
+(
+	id INT,
+	email VARCHAR
+);
+
+create table goals
+(
+	id INT,
+	name VARCHAR,
+	description VARCHAR
+);
+
+INSERT INTO users
+VALUES
+	(1, 'Hello@mail.de');
