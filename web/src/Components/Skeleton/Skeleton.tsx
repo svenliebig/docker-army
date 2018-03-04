3e390279bc2b9fa51b8dcb00cbab86ba66fcae4d
Sven Liebig <liebigsv@gmail.com>
2018-03-04 21:31:03 +0100

    appbar is implemented

--- web/src/Components/Skeleton/Skeleton.tsx
+++ web/src/Components/Skeleton/Skeleton.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { withStyles, AppBar, Toolbar, IconButton, MenuItem, Button, Icon, StyledComponentProps, WithStyles, Drawer, Theme } from "material-ui";
+import { withStyles, AppBar, Toolbar, IconButton, MenuItem, Button, Icon, StyledComponentProps, WithStyles, Drawer, Theme, Divider } from "material-ui";
 
 export interface Props extends StyledComponentProps {
     children?: React.ReactNode
@@ -11,15 +11,59 @@ export interface State {
 
 
 
+const drawerWidth = 240;
 const styles = (theme: Theme) => ({
+    root: {
+        flexGrow: 1,
+        display: "flex"
+    },
     appBar: {
-        zIndex: theme.zIndex.drawer + 1
+        position: 'absolute',
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        })
+    },
+    appBarShift: {
+        width: `calc(100% - ${drawerWidth}px)`,
+        marginLeft: drawerWidth,
+        transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    menuButton: {
+        color: "white"
+    },
+    drawerHeader: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'flex-end',
+        padding: '0 8px',
+        ...theme.mixins.toolbar,
     },
     drawerPaper: {
-        position: ("relative" as any),
-        width: 240,
+        position: "relative",
+        width: drawerWidth,
     },
-    toolbar: theme.mixins.toolbar
+    toolbar: theme.mixins.toolbar,
+    content: {
+        flexGrow: 1,
+        marginLeft: -drawerWidth,
+        backgroundColor: theme.palette.background.default,
+        padding: theme.spacing.unit * 3,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+    },
+    contentShift: {
+        marginLeft: 0,
+        transition: theme.transitions.create('margin', {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    }
 })
 
 class Skeleton extends React.Component<Props & WithStyles<"appBar" | "toolbar" | "drawerPaper">, State> {
@@ -41,26 +85,48 @@ class Skeleton extends React.Component<Props & WithStyles<"appBar" | "toolbar" |
         const { classes } = this.props
         return (
             <div className={this.props.classes.root}>
-                <AppBar className={this.props.classes.appBar}>
+                <AppBar className={this.appbarClassNames} color="primary">
                     <Toolbar>
                         <IconButton className={this.props.classes.menuButton} onClick={this.toggleSidemenu.bind(this)}>
-                            <Icon>star</Icon>
+                            <Icon>dehaze</Icon>
                         </IconButton>
                         <Button color="inherit">Login</Button>
                     </Toolbar>
                 </AppBar>
                 <Drawer anchor="left" variant="persistent" open={this.state.open} classes={{
-                    paper: this.props.classes.drawerPaper,
+                    paper: classes.drawerPaper,
                 }}>
-                    <div className={classes.toolbar} />
+                    <div className={classes.drawerHeader} />
+                    <Divider />
                     Ello
                 </Drawer>
-                <div className="content">
+                <div className={this.contentClassNames}>
+                    <div className={classes.drawerHeader} />
                     {this.props.children}
                 </div>
             </div>
         )
     }
+
+    private get appbarClassNames(): string {
+        const classNames: Array<string> = [this.props.classes.appBar]
+
+        if (this.state.open) {
+            classNames.push(this.props.classes.appBarShift)
+        }
+
+        return classNames.filter(e => e).join(" ")
+    }
+
+    private get contentClassNames(): string {
+        const classNames: Array<string> = [this.props.classes.content]
+
+        if (this.state.open) {
+            classNames.push(this.props.classes.contentShift)
+        }
+
+        return classNames.filter(e => e).join(" ")
+    }
 }
 
-export default withStyles(styles)<Props>(Skeleton)
+export default withStyles(styles as any)<Props>(Skeleton)
