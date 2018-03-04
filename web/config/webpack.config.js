362423b747f807fed0b098003947f4ea99b84418
Sven Liebig <liebigsv@gmail.com>
2018-03-04 16:18:49 +0100

    implement jest

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -4,7 +4,7 @@ const root = path.resolve(__dirname, "..")
 const src = path.resolve(root, "src")
 const dist = path.resolve(root, "dist")
 const config = path.resolve(root, "config")
-const tsconfig = path.resolve(config, "tsconfig.json")
+const tsconfig = path.resolve(root, "tsconfig.json")
 const indexts = path.resolve(src, "index.tsx")
 
 module.exports = {
