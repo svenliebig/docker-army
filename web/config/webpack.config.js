d9eaf3ae234fb9bde3d82b929132d57974a26c45
Sven Liebig <liebigsv@gmail.com>
2018-03-04 15:26:47 +0100

    ignore bundle js

--- web/config/webpack.config.js
+++ web/config/webpack.config.js
@@ -3,12 +3,31 @@ const path = require('path')
 const root = path.resolve(__dirname, "..")
 const src = path.resolve(root, "src")
 const dist = path.resolve(root, "dist")
-const indexJs = path.resolve(src, "index.jsx")
+const config = path.resolve(root, "config")
+const tsconfig = path.resolve(config, "tsconfig.json")
+const indexts = path.resolve(src, "index.tsx")
 
 module.exports = {
-	entry: [indexJs],
+	mode: "development",
+	entry: [indexts],
+	devtool: 'inline-source-map',
+	module: {
+		rules: [{
+			test: /\.tsx?$/,
+			use: [{
+				loader: 'ts-loader',
+				options: {
+					configFile: tsconfig
+				}
+			}],
+			exclude: /node_modules/
+		}]
+	},
+	resolve: {
+		extensions: ['.tsx', '.ts', '.js']
+	},
 	output: {
 		path: dist,
 		filename: 'bundle.js'
-	}
+	},
 }
