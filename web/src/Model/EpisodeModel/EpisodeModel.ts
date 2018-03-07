7b3d8ea85eaad5adce42f0176c74d95c025e0e53
Sven Liebig <liebigsv@gmail.com>
2018-03-08 00:08:08 +0100

    create a fully tested model with unfreeze function

--- web/src/Model/EpisodeModel/EpisodeModel.ts
+++ web/src/Model/EpisodeModel/EpisodeModel.ts
@@ -1,18 +1,69 @@
+/** 
+ * Represents an EpisodeModel with a name, episode, watched flag and an airdate.
+ */
 export default class EpisodeModel {
     public name: string
-    public airdate: Date
-    public season: number
     public episode: number
     public watched: boolean
+    public airdate: Date | null
 
     constructor() {
+        this.airdate = null
     }
 
-    static unfreeze(json: string): EpisodeModel | null {
+    /**
+     * Parses a JSON String or a Object to an EpisodeModel. If name or episode
+     * is not defined, the return value will be null. If a json parse error happens
+     * the return will be null.
+     * 
+     * @param data The Json string or the data object to parse.
+     * 
+     * @returns null or an EpisodeModel
+     */
+    public static unfreeze(data: string | object): EpisodeModel | null {
+        if (typeof data === "string")
+            return EpisodeModel.unfreezeJson(data)
+        else if (typeof data === "object")
+            return EpisodeModel.unfreezeObject(data)
+        else
+            return null
+    }
+
+    private static unfreezeJson(json: string): EpisodeModel | null {
         if (!json)
             return null
 
+        let parsedObject
+        try {
+            parsedObject = JSON.parse(json)
+        } catch (e) {
+            console.debug(`Couldn't parse ${EpisodeModel.name}.`, e)
+            return null
+        }
+
+        return EpisodeModel.unfreezeObject(parsedObject)
+    }
+
+    private static unfreezeObject(data: any): EpisodeModel | null {
+        if (!data)
+            return null
+
+        const { name, episode, watched, airDate } = data
+
+        if (!name || !episode)
+            return null
+
         const model = new EpisodeModel()
+
+        model.name = name
+        model.episode = episode
+        model.watched = watched || false
+        if (airDate) {
+            const parsedAirdate = Date.parse(airDate)
+            if (parsedAirdate) {
+                model.airdate = new Date(parsedAirdate)
+            }
+        }
 
         return model
     }
--- web/test/Model/EpisodeModel.test.ts
+++ web/test/Model/EpisodeModel.test.ts
@@ -2,13 +2,13 @@ import EpisodeModel from "../../src/Model/EpisodeModel"
 
 describe(EpisodeModel.name, () => {
 
-	describe(`${EpisodeModel.unfreeze.name}(json: string): ${EpisodeModel.name}`, () => {
+	describe(`${EpisodeModel.unfreeze.name}(data: string): ${EpisodeModel.name}`, () => {
 
 		beforeAll(() => {
 		})
 
 		const nullCase = (value: string) => {
-			describe(`json = ${typeof value === "object" ? value : `"${value}"`}`, () => {
+			describe(`data = ${typeof value === "object" ? value : `"${value}"`}`, () => {
 				it(`should return ${null}`, () => {
 					const model: EpisodeModel = EpisodeModel.unfreeze(value)
 					expect(model).toBeNull()
@@ -17,10 +17,100 @@ describe(EpisodeModel.name, () => {
 		}
 
 		nullCase(null)
-		nullCase("")
-		nullCase("null")
-		nullCase("{}")
+		nullCase(undefined)
+		nullCase(``)
+		nullCase(`null`)
+		nullCase(`{}`)
+		nullCase(`{ "name": "a episode name" }`)
+		nullCase(`{ "name": "a episode name", "watched": true }`)
+		nullCase(`{ "name": "a episode name", "airDate": "12-12-12" }`)
+		nullCase(`{ "airDate": "12-12-12" }`)
+		nullCase(`{ "watched": true }`)
+		nullCase(`{ "episode": 2 }`)
+		nullCase(`{ "episode": 2, "watched": true }`)
+		nullCase(`{ "episode": 2, "airDate": "12-12-12" }`)
+		nullCase(JSON.parse(`{ "name": "a episode name", "airDate": "12-12-12" }`))
 
+		const nameEpisodeCase = () => {
+			const value: any = { name: "a_name", episode: 1 }
+			const stringifiedValue = JSON.stringify(value)
+			describe(`data = "${stringifiedValue}"`, () => {
+				it(`should return a model with name = ${value.name}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.name).toBe(value.name)
+				})
+				it(`should return a model with episode = ${value.episode}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.episode).toBe(value.episode)
+				})
+				it(`should return a model with watched = ${false}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.watched).toBe(false)
+				})
+				it(`should return a model with airdate = ${null}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.airdate).toBeNull()
+				})
+			})
+		}
+		nameEpisodeCase()
+
+		const nameEpisodeWatchedCase = () => {
+			const value = { name: "a_name", episode: 1, watched: true }
+			const stringifiedValue = JSON.stringify(value)
+			describe(`data = "${stringifiedValue}"`, () => {
+				it(`should return a model with watched = ${value.watched}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.watched).toBe(value.watched)
+				})
+				it(`should return a model with airdate = ${null}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.airdate).toBeNull()
+				})
+			})
+		}
+		nameEpisodeWatchedCase()
+
+		const nameEpisodeAirDateCase = () => {
+			const value = { name: "a_name", episode: 1, airDate: "2012-10-24" }
+			const stringifiedValue = JSON.stringify(value)
+			describe(`data = "${stringifiedValue}"`, () => {
+				it(`should return a model with airdate = ${value.airDate}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.airdate).toEqual(new Date(Date.parse(value.airDate)))
+				})
+			})
+		}
+		nameEpisodeAirDateCase()
+
+		const nameEpisodeUnusuableAirDateCase = () => {
+			const value = { name: "a_name", episode: 1, airDate: "asdfwhat" }
+			const stringifiedValue = JSON.stringify(value)
+			describe(`data = "${stringifiedValue}"`, () => {
+				it(`should return a model with airdate = ${null}`, () => {
+					const model: EpisodeModel = EpisodeModel.unfreeze(stringifiedValue)
+					expect(model.airdate).toBeNull()
+				})
+			})
+		}
+		nameEpisodeUnusuableAirDateCase()
+
+		describe(`data = "{ { \\ }}}}"`, () => {
+			let debugSpy: jasmine.Spy
+
+			beforeEach(() => {
+				debugSpy = spyOn(console, "debug")
+			})
+
+			it(`should return null after data parse error`, () => {
+				const model: EpisodeModel = EpisodeModel.unfreeze("{ { \\ }}}}")
+				expect(model).toBeNull()
+			})
+
+			it(`should call console.debug 1 time`, () => {
+				const model: EpisodeModel = EpisodeModel.unfreeze("{ { \\ }}}}")
+				expect(debugSpy).toHaveBeenCalledTimes(1)
+			})
+		})
 	})
-
 })
